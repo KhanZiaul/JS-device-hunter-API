543f32e4fd08ce82f13ccb1a2a240e3bafe682c6
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-02-26 13:26:37 +0600

    show hide data done

--- js/app.js
+++ js/app.js
@@ -1,4 +1,8 @@
+// show hide datas input initial value
+
 let searchText = "";
+
+// show limited data function
 
 const loadData = async (searchInput) => {
 
@@ -10,6 +14,9 @@ const loadData = async (searchInput) => {
 
     showDatas(datas.data);
 }
+
+// show limited datas innerHTML
+
 
 function showDatas(datas) {
 
@@ -81,33 +88,95 @@ function showDatas(datas) {
 }
 
 
-    document.getElementById('search-button').addEventListener('click', function () {
+// show hide datas innerHTML
 
-        const searchInput = document.getElementById('search-input').value;
 
-        searchText = searchInput;
+function showHideDatas(datas) {
 
-        console.log(searchText);
+    const showPhones = document.getElementById('showPhones');
 
-        loadData(searchInput);
+    const errorMessage = document.getElementById('error-message');
 
-        isSpinner(true);
+    if (datas.length === 0) {
 
-        document.getElementById('search-input').value = "";
+        errorMessage.classList.remove('hidden');
+    }
+
+    else {
+
+        errorMessage.classList.add('hidden');
+
+    }
+
+    showPhones.innerText = "";
+
+    datas.forEach(data => {
+
+        const createDiv = document.createElement('div');
+
+        createDiv.classList.add('border-2');
+
+        createDiv.classList.add('p-5');
+
+        createDiv.classList.add('bg-sky-200');
+
+        createDiv.classList.add('rounded-xl');
+
+        createDiv.innerHTML = `
+        <figure class="px-10 pt-10">
+                  <img src= ${data.image} alt="Shoes" class="rounded-xl" />
+                </figure>
+                <div class="card-body items-center text-center">
+                  <h2 class="card-title mb-2"> ${data.phone_name}</h2>
+                  <p class="mb-2">${data.slug}</p>
+                  <div class="card-actions">
+                    <button class="btn btn-primary">Buy Now</button>
+                  </div>
+        `;
+
+        showPhones.appendChild(createDiv);
+
+        console.group(data.slug);
 
     });
 
-const showAll = () => {
+    isSpinner(false);
+
+}
+
+// get search input value
+
+document.getElementById('search-button').addEventListener('click', function () {
+
+    const searchInput = document.getElementById('search-input').value;
+
+    searchText = searchInput;
+
+    loadData(searchInput);
+
+    isSpinner(true);
+
+    document.getElementById('search-input').value = "";
+
+});
+
+
+// show hide datas fuction
+
+function showAll() {
 
     url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
 
     fetch(url)
         .then(res => res.json())
-        .the(datas => showDatas(datas));
+        .then(datas => showHideDatas(datas.data));
+
+    document.getElementById('showBtn').classList.add('hidden');
+
 }
 
 
-
+// spinner function
 
 const spinner = document.getElementById('spinner');
 
@@ -123,5 +192,3 @@ function isSpinner(isSpin) {
         spinner.classList.add('hidden');
     }
 }
-
-// loadData();
